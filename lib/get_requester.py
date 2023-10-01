c08c82a285c4df4d08f153774a69981b3a7da217
Bitutu <maryanne.osoro@student.moringaschool.com>
2023-10-01 09:24:19 +0300

    DONE

---
+++ lib/get_requester.py
@@ -0,0 +1,19 @@
+import requests
+import json
+
+class GetRequester:
+
+    def __init__(self, url):
+        self.url = url
+
+    def get_response_body(self):
+        response = requests.get(self.url)
+        return response.text
+
+    def load_json(self):
+        response_body = self.get_response_body()
+        try:
+            return response_body.json()
+        except ValueError:
+
+            return None
--- lib/testing/get_requester_test.py
+++ lib/testing/get_requester_test.py
@@ -1,16 +1,17 @@
-from GetRequester import GetRequester
-class GetRequesterTest:
-    '''Class {Classname} in {modulename}.py'''
+from lib import GetRequester
+
 URL = 'https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json'
 JSON_STRING = b"[\n  {\n    \"name\": \"Daniel\",\n    \"occupation\": \"LG Fridge Salesman\"\n  },\n  {\n    \"name\": \"Joe\",\n    \"occupation\": \"WiFi Fixer\"\n  },\n  {\n    \"name\": \"Avi\",\n    \"occupation\": \"DJ\"\n  },\n  {\n    \"name\": \"Howard\",\n    \"occupation\": \"Mountain Legend\"\n  }\n]\n"
 CONVERTED_DATA = [{ 'name': 'Daniel', 'occupation' : 'LG Fridge Salesman' }, { 'name': 'Joe', 'occupation': 'WiFi Fixer' }, { 'name': 'Avi', 'occupation': 'DJ' }, { 'name': 'Howard', 'occupation': 'Mountain Legend' }]
 
 def test_get_response():
-        '''get_response_body function returns response.'''
-        requester = GetRequester(URL)
-        assert(requester.get_response_body() == JSON_STRING)
+    '''get_response_body function returns response.'''
+    requester = GetRequester(URL)
+    response = requester.get_response_body().decode() 
+    assert response == JSON_STRING
 
 def test_load_json():
-        '''load_json function returns response.'''
-        requester = GetRequester(URL)
-        assert(requester.load_json() == CONVERTED_DATA)
+    '''load_json function returns response.'''
+    requester = GetRequester(URL)
+    assert requester.load_json() == CONVERTED_DATA
+
